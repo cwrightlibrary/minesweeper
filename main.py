fbca1be73c887118154dff8d298966e6e66ec8ca
Chris Wright <cwright@richlandlibrary.com>
2025-03-06 11:51:20 -0500

    added ref and new button grid

--- main.py
+++ main.py
@@ -4,13 +4,24 @@ class App(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("minesweeper")
-        self.geometry("450x450")
+        self.geometry("470x620")
+
         self.center_window()
         self.focus_force()
 
-        self.buttons = []
+        self.grid_rowconfigure((0, 1), weight=1)
+        self.grid_columnconfigure(0, weight=1)
 
-        self.create_grid()
+        self.header = tk.Frame(self, width=450, height=250, background="red")
+        self.header.grid(row=0, column=0, pady=(10, 5))
+        self.header.grid_rowconfigure(0, weight=1)
+        self.header.grid_columnconfigure((0, 1, 2), weight=1)
+
+        self.game_area = tk.Frame(self, width=450, height=450)
+        self.game_area.grid(row=1, column=0, padx=10, pady=(5, 10))
+
+        self.buttons = []
+        self.add_buttons()
 
     def center_window(self):
         self.update_idletasks()
@@ -18,13 +29,16 @@ class App(tk.Tk):
         scr_width, scr_height = self.winfo_screenwidth(), self.winfo_screenheight()
         x, y = (scr_width - width) // 2, (scr_height - height) // 2
         self.geometry(f"{width}x{height}+{x}+{y}")
-
-    def create_grid(self):
-        for x in range(0, 450, 50):
-            for y in range(0, 450 ,50):
-                btn = tk.Frame(self, relief="raised", borderwidth=5, width=50, height=50)
-                btn.place(x=x, y=y)
+    
+    def add_buttons(self):
+        for i in range(10):
+            for j in range(10):
+                btn = tk.Button(self.game_area, text="", relief="raised", width=20, height=3, borderwidth=5)
+                btn.grid(row=i, column=j, sticky="nsew")
                 self.buttons.append(btn)
+        for i in range(10):
+            self.game_area.grid_rowconfigure(i, weight=1)
+            self.game_area.grid_columnconfigure(i, weight=1)
 
 
 if __name__ == "__main__":
